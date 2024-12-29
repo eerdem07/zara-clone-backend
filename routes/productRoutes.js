a3f6c4aad2e0a378c51b65540901304c3b2d9944
Emre Erdem <emreerdem502@gmail.com>
2024-12-30 00:40:41 +0300

    updated

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,13 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 const productController = require("../controllers/productController");
-const authController = require('../controllers/authController')
+const authController = require("../controllers/authController");
 
 router
   .route("/")
   .get(productController.getAllProduct)
-  .post(authController.authorize(['employee','admin']), productController.addProduct)
-  .patch(authController.authorize(['employee','admin']), productController.updateProduct);
+  .post(
+    authController.authorize(["employee", "admin"]),
+    productController.addProduct
+  )
+  .patch(
+    authController.authorize(["employee", "admin"]),
+    productController.updateProduct
+  );
 
 // getAllProduct -> herkes
 // addProduct -> Manager, admin
@@ -17,7 +23,10 @@ router
 router
   .route("/:id")
   .get(productController.getProduct)
-  .delete(authController.authorize(['employee','admin']),productController.deleteProduct);
+  .delete(
+    authController.authorize(["employee", "admin"]),
+    productController.deleteProduct
+  );
 
 // getProduct -> herkes
 // deleteProduct -> admin, deleteProduct
